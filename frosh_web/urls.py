8909e520e942446e7d6c9439ddcd75a99b4261f4
Pancham Agarwal <pancham1603@gmail.com>
2023-07-18 03:42:09 +0530

    modified project structure

--- frosh_web/urls.py
+++ frosh_web/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views
+from apps.users import views
 
 urlpatterns = [
     path('', views.home),
     path('admin/', admin.site.urls),
     path('login/', views.login_user),
     path('logout/', views.logout_user),
-    path('users/', include("users.urls")),
+    path('users/', include("apps.users.urls")),
     # path('users/', include("django.contrib.auth.urls")),
 ]
