eb7d0b0636a13e7944f71a376286855ce82a4cbf
Pancham Agarwal <pancham1603@gmail.com>
2023-07-19 17:21:28 +0530

    added instructions for event passes

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, HttpResponse
-from .models import Event
+from .models import Event, EventPass
 
 # Create your views here.
 def events_home(request):
     events = Event.objects.all()
     return render(request, 'events.html', {'events':events})
 
+
+#- /events/register/<event id>
+#- event ID from 'django dynamic URL patterns'
+#- user ID from request.session.get("user")
+#- passId will be random alphanimeric, generated using Random module
+#- QR code will be made from Pass ID using pyzbar and pypng
