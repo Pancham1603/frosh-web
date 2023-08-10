2bb7c04df223a4dce44ab6a83bd27b345d5167df
Pancham Agarwal <pancham1603@gmail.com>
2023-08-10 18:47:19 +0530

    code cleanup

--- apps/events/views.py
+++ apps/events/views.py
@@ -8,15 +8,20 @@ from django.template.loader import render_to_string
 from .models import Event, EventPass, EventSlot
 from ..users.models import User
 import random
-import png
-import pyqrcode
-import base64
-import requests
 import string
-import os
 import json
 from datetime import date, time, datetime
 from decouple import config
+
+from ..users.utils import account_activation_token
+import random, string
+import openpyxl
+from .models import User
+from ..users.manager import generate_qr, generate_user_secure_id
+from ..users.views import *
+from datetime import datetime
+import json
+
 
 global counters
 counters = {} 
@@ -25,7 +30,7 @@ for event in Event.objects.all():
 for slot in EventSlot.objects.all():
     counters[slot.slot_id] = slot.passes_generated
 
-# Create your views here.
+
 @login_required
 def events_home(request):
     events = Event.objects.filter(is_display=True)
@@ -55,32 +60,6 @@ def events_home(request):
     user_passes = sorted(user_passes, key=lambda x: [x.event_id.date])[::-1]
 
     return render(request, 'events.html', {'upcoming_events':events_sorted, 'past_events':past_events, 'events_today':events_today, 'event_slots':event_slots, 'user_passes':user_passes, 'live_event':live_event})			  
-
-
-    # user_passes = EventPass.objects.filter(user_id=request.user)
-
-    # scheduled_events = events
-
-    # # try:
-    # #     live_event = events_sorted[0]
-    # #     live_event_pass = user_passes.filter(event_id=live_event, user_id=request.user).first()
-    # #     scheduled_events = events.exclude(event_id=live_event.event_id)
-    # # except:
-    # #     live_event = None
-    # #     live_event_pass = None
-    # #     scheduled_events = events
-    # for Pass in user_passes:
-    #     if Pass.event_id in events:
-    #       scheduled_events = scheduled_events.exclude(event_id=Pass.event_id.event_id)
-    #     else:
-    #         user_passes=user_passes.exclude(event_id=Pass.event_id)
-    # event_slots = EventSlot.objects.all()
-    # user_passes_all = user_passes
-    # # if live_event:
-    # #     user_passes = user_passes.exclude(event_id=live_event.event_id)
-    # scheduled_events = sorted(scheduled_events, key=lambda x: [x.date, convert_time_to_start_time(x.time)])
-    # # return render(request, 'events.html', {'live_event':live_event,'live_event_pass':live_event_pass, 'scheduled_events':scheduled_events ,'user_passes':user_passes, 'event_slots':event_slots, 'user_passes_all':user_passes_all})
-    # return render(request, 'events.html', {'scheduled_events':scheduled_events ,'user_passes':user_passes, 'event_slots':event_slots, 'user_passes_all':user_passes_all})
 
 
 @csrf_exempt
@@ -279,11 +258,6 @@ def generate_pass(request, event_id, slot_id=None):
                 }
         return HttpResponse(json.dumps(data))
 
-def generate_qr(value):
-    qr = pyqrcode.create(value)
-    qr.png(f'{value}.png', scale=8)
-    return f'{value}.png'
-
 
 def confirmation_email(generated_pass:EventPass):
     subject= subject = f'Registration successful for {generated_pass.event_id} | Frosh 23'
@@ -305,25 +279,75 @@ def convert_time_to_start_time(time_string):
     return start_time
 
 
-# def test_functions():
-#     active_users = User.objects.filter(is_active=True)
-#     staff_users = User.objects.filter(is_staff=True)
-#     superusers = User.objects.filter(is_superuser=True)
-#     print('Active Users: ',active_users.count())
-#     print('Staff Users: ',staff_users.count())
-#     print('Superusers: ',superusers.count())
-
-# test_functions()
+def user_count():
+    active_users = User.objects.filter(is_active=True)
+    staff_users = User.objects.filter(is_staff=True)
+    superusers = User.objects.filter(is_superuser=True)
+    print('Active Users: ',active_users.count())
+    print('Staff Users: ',staff_users.count())
+    print('Superusers: ',superusers.count())
 
 
-# def tslas_checkup():
-#     all_users = EventPass.objects.filter(event_id=Event.objects.get(event_id='Whodunit [Main Audi]@Frosh23'))
-#     for user in all_users:
-#         if user.user_id.email.find('blas23@thapar.edu') != -1:
-#             print(f"[Main Auditorium] {user.user_id.registration_id} {user.user_id} {user.user_id.email}")
-#     all_users = EventPass.objects.filter(event_id=Event.objects.get(event_id='Whodunit [Tan Audi]@Frosh23'))
-#     for user in all_users:
-#         if user.user_id.email.find('blas23@thapar.edu') != -1:
-#             print(f"[Tan Auditorium] {user.user_id.registration_id} {user.user_id} {user.user_id.email}")
+def tslas_checkup():
+    all_users = EventPass.objects.filter(event_id=Event.objects.get(event_id='Whodunit [Main Audi]@Frosh23'))
+    for user in all_users:
+        if user.user_id.email.find('blas23@thapar.edu') != -1:
+            print(f"[Main Auditorium] {user.user_id.registration_id} {user.user_id} {user.user_id.email}")
+    all_users = EventPass.objects.filter(event_id=Event.objects.get(event_id='Whodunit [Tan Audi]@Frosh23'))
+    for user in all_users:
+        if user.user_id.email.find('blas23@thapar.edu') != -1:
+            print(f"[Tan Auditorium] {user.user_id.registration_id} {user.user_id} {user.user_id.email}")
 
-# tslas_checkup()
+
+def check_count():
+    while True:
+        print(EventPass.objects.filter(event_id=Event.objects.get(event_id='Event X@Frosh23')).count())
+
+
+def generate_password():
+    user = User.objects.get(registration_id='141208')
+    user.set_password('12345678')
+    print(user)
+
+
+def gen_special_passed():
+    count = 0
+    wb_obj = openpyxl.load_workbook()
+    sheet = wb_obj.active
+    start = int(input("Start: "))
+    end = int(input('End: '))
+    event = Event.objects.get(event_id='Event X@Frosh23')
+    print(event)
+    for row in range(start, end+1):
+        data = []
+        user = User()
+        for column in range(1,7):
+            cell = sheet.cell(row=row, column=column)
+            data.append(cell.value)
+        user = User.objects.create_user(registration_id=int(data[0]),first_name = data[1], last_name = data[3] if data[3] else ' ',
+        email = data[4], is_staff = False, is_superuser = False, is_active = False, image=data[5])
+        count+=1
+        user.secure_id = generate_user_secure_id()
+        user.qr = upload_qr(generate_qr(json.dumps({
+                'registration_id':user.registration_id,
+                'secure_id':user.secure_id
+            }), user.registration_id), user.secure_id)
+        print(f"[{count}] ",f"[ADDED TO DATABASE] {user.first_name}")
+        while True:
+            pass_id = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=16))
+
+            if not EventPass.objects.filter(pass_id=pass_id).count():
+                break
+
+        generated_pass = EventPass(event_id=event, pass_id=pass_id, user_id=user)
+        generated_pass.qr = user.qr
+        generated_pass.time = event.time
+        generated_pass.save()
+        
+        confirmation_email(generated_pass=generated_pass)
+        print(pass_id, 'emailed')
+        event.passes_generated = counters[event.name]
+        user.events.append(str(event))
+        user.save()
+        event.save()
