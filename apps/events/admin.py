86808d0d25cc94104d894840403c109f7201b40e
Pancham Agarwal <pancham1603@gmail.com>
2023-08-17 13:26:50 +0530

    search in passes table enabled

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -4,13 +4,13 @@ from .models import *
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date','time', 'venue', 'max_capacity', 'passes_generated', 'is_booking', 'is_display')
+    list_display = ('name', 'date','time', 'venue', 'max_capacity', 'passes_generated', 'is_booking', 'is_display', 'booking_complete', 'slots_required')
     search_fields = ('name', 'date', 'venue')
 
 
 class EventPassAdmin(admin.ModelAdmin):
-    list_display = ('pass_id', 'user_id', 'event_id')
-    search_fields = ('pass_id', 'user_id', 'event_id')
+    list_display = ('pass_id', 'user_id', 'event_id', 'slot_id')
+    search_fields = ('pass_id', 'user_id__first_name','user_id__registration_id', 'event_id__name', 'event_id__slot_id')
 
 class EventSlotAdmin(admin.ModelAdmin):
     list_display = ('event','date','time', 'venue', 'max_capacity', 'passes_generated')
